f13bc35a86af6ef69676b482e5de8406c724a0bf
santosh09 <santosh09@gmail.com>
2025-01-27 17:25:00 +0000

    Update main.tf with new EC2 instance configurations

--- main.tf
+++ main.tf
@@ -32,3 +32,13 @@ module "ec2_instance_sachin" {
     Name = "sachin"
   }
 }
+module "ec2_instance_santosh1" {
+  source        = "./ec2_instance"
+  region        = "us-west-2"
+  ami           = "ami-0fa40e25bf4dda1f6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "santosh1"
+  }
+}
