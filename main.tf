20fa79fc484e9e9512980a9a0a49e034197fa2c4
santosh09 <santosh09@gmail.com>
2025-01-27 14:27:17 +0000

    Update main.tf with new EC2 instance configurations

--- main.tf
+++ main.tf
@@ -22,3 +22,13 @@ module "ec2_instance_ExampleInstance12" {
     Name = "ExampleInstance12"
   }
 }
+module "ec2_instance_rupesh1" {
+  source        = "./ec2_instance"
+  region        = "us-west-2"
+  ami           = "ami-0fa40e25bf4dda1f6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "rupesh1"
+  }
+}
